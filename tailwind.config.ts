383990d74492bc2e0c2a89ddcdb3e21d9c64090c
Trevor Aupperle <tza0038@auburn.edu>
2023-09-17 19:42:06 -0500

    Landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,18 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      crimsonPro: ["Crimson Pro", "serif"],
+    },
+    extend: {
+      colors: {
+        "se-teal": "#264653",
+        "se-aqua": "#2A9D8F",
+        "se-yellow": "#E9C46A",
+        "se-orange": "#F4A261",
+        "se-red": "#E76F51",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
